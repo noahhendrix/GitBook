7898d4224d5baeb29e9fee2ed4bc3096f14f1531
Noah Hendrix <noahhendrix@gmail.com>
2011-06-23 02:50:28 -0500

    create repositories from their slug (username/repo_name), create user if they don't exist, and validate that the repo exists on github

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -3,4 +3,36 @@ class Repository < ActiveRecord::Base
   #associations
     belongs_to :user
   
+  #attributes
+    def slug
+      [user.name, name].join('/')
+    end
+    alias_method :to_param, :slug
+  
+  #class methods
+    def self.find_by_username_and_name(username, name)
+      joins(:user).where('users.name = ? AND repositories.name = ?', username, name).first
+    end
+    
+    def self.create_by_username_and_name(username, name)
+      user = User.find_or_create_by_name(username)
+      repo = build_from_github("#{username}/#{name}")
+      user.repositories << repo
+      repo
+    end
+    
+    def self.find_or_create(username, name)
+      begin
+        find_by_username_and_name(username, name) || create_by_username_and_name(username, name)
+      rescue Octokit::NotFound => e
+        raise ActiveRecord::RecordNotFound
+      end
+    end
+  
+  private
+    def self.build_from_github(slug)
+      github_info = Octokit.repo(slug)
+      new(name: github_info[:name], description: github_info[:description], url: github_info[:url])
+    end
+  
 end
