1c5e3b86d89f050a23b3a641f1dfc6c75ab652a3
Noah Hendrix <noahhendrix@gmail.com>
2011-06-23 01:32:17 -0500

    also collect user's email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
   #class methods
     def self.create_with_omniauth(auth)
       create! do |user|
-        user.provider = auth["provider"]
-        user.uid = auth["uid"]
-        user.name = auth["user_info"]["name"]
+        user.provider = auth['provider']
+        user.uid = auth['uid']
+        user.name = auth['user_info']['nickname']
+        user.email = auth['user_info']['email']
       end
     end
   
