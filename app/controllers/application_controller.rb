c7d48952010b10deeb402d646927c0eb851f89ac
Noah Hendrix <noahhendrix@gmail.com>
2011-06-23 01:36:51 -0500

    anticipate future globbing of routes so pass omniauth urls to a 404 page so they can still work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   
+  def omniauth
+    #a blank method used to allow omniauth urls to pass
+    #without be routed as username/respoitory
+    render text: 'Authentication', status: 404
+  end
+  
   private
   
   def current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Gitbook::Application.routes.draw do
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/signout' => 'sessions#destroy', as: :signout
+  
+  match 'auth/*rest' => 'application#omniauth'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
