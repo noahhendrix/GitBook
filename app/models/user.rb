608464209f9deb4e73293182276cbad26ecd1ad0
Noah Hendrix <noahhendrix@gmail.com>
2011-06-23 01:35:11 -0500

    give user avatar_url attr so it can be displayed with their name on the site

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ActiveRecord::Base
   
   #associations
     has_many :repositories
+  
+  #attributes
+    def avatar_url(size=48)
+      gravatar_id = Digest::MD5.hexdigest(email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(DEFAULT_AVATAR_URL)}"
+    end
+  
+  #constants
+    DEFAULT_AVATAR_URL = '/images/guest.png'
+  
   #class methods
     def self.create_with_omniauth(auth)
       create! do |user|
