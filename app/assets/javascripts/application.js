05f98ba4a2dc7744ac11ab9bc5a3143ebc9e7d0e
Noah Hendrix <noahhendrix@gmail.com>
2011-06-24 18:09:55 -0500

    keyboard navigation to move through events and 'focus' them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,34 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+  var $events = $('.event');
+  
+  //touch event to focus it
+  $events.click(function(){
+    $events.removeClass('focused');
+    $(this).addClass('focused');
+  });
+  
+  //keyboard navigation through events
+  $(document.documentElement).keyup(function(event) {
+    var $selected = $events.filter('.focused').first();
+    if ($selected.length == 0)
+      $selected = $events.first();
+
+    $selected.removeClass('focused');
+    
+    var $next;
+    
+    if (event.keyCode == 74 || event.keyCode == 40)
+      $next = $selected.next('.event');
+    if (event.keyCode == 75 || event.keyCode == 38)
+      $next = $selected.prev('.event');
+      
+    $next.addClass('focused');
+    $.scrollTo(($next), {
+      duration: 500
+    });
+  });
+});
